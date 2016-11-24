a05b9d63fe2c472b23e558e41e3fa8cbab7b1850
David Lim <nicholai.david@gmail.com>
2016-11-24 21:02:52 +0800

    Remove comments when viewing a single post

--- single.php
+++ single.php
@@ -21,14 +21,14 @@
 
             <?php get_template_part( 'loop-templates/content', 'single' ); ?>
 
-            <?php the_post_navigation(); ?>
+            <!-- <?php the_post_navigation(); ?> -->
 
-            <?php
+            <!-- <?php
               // If comments are open or we have at least one comment, load up the comment template
-              if ( comments_open() || get_comments_number() ) :
-                  comments_template();
-              endif;
-            ?>
+              // if ( comments_open() || get_comments_number() ) :
+              //     comments_template();
+              // endif;
+            ?> -->
 
           <?php endwhile; // end of the loop. ?>
 
